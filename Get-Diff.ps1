798fb1de05047d02bb5cfa9293a9c56c480f4840
William Boyd <wboyd@boyd-tech.com>
2024-11-25 08:53:39 -0600

    rename to Get-Diff, and add support for pscustomobject

--- Get-Diff.ps1
+++ Get-Diff.ps1
@@ -1,4 +1,4 @@
-function Compare-ObjectGitDiff {
+function Get-Diff {
     <#
     .SYNOPSIS
         Returns the difference between two objects (as json) in a git diff format
@@ -7,7 +7,8 @@ function Compare-ObjectGitDiff {
     .PARAMETER DifferenceObject
         The object used to compare against the reference object and show changes
     .EXAMPLE
-        PS> Compare-ObjectGitDiff -ReferenceObject @{"yo"="wassup"} -DifferenceObject @{"yo"="sup"}
+        PS> $proc = Get-Process
+        PS> Get-Diff -ReferenceObject $proc[0] -DifferenceObject $proc[1]
     #>
     [CmdletBinding()]
     param(
@@ -29,7 +30,8 @@ function Compare-ObjectGitDiff {
             if ($null -eq $Object) {
                 return
             }
-            if ($Object -is [hashtable]) {
+
+            if ($Object -is [hashtable] -or $Object -is [ordered]) {
                 $sortedHashtable = [ordered]@{}
                 foreach ($key in $Object.Keys | Sort-Object) {
                     if ($Object[$key]) {
@@ -38,18 +40,28 @@ function Compare-ObjectGitDiff {
                 }
                 return $sortedHashtable
             }
-            elseif ($Object -is [array]) {
+
+            if ($Object.GetType().Name -eq 'PSCustomObject') {
+                $sortedHashtable = [ordered]@{}
+                foreach ($key in $Object.PSObject.Properties.Name | Sort-Object) {
+                    if ($Object.$key) {
+                        $sortedHashtable[$key] = Format-ObjectForDiff -Object $Object.$key
+                    }
+                }
+                return $sortedHashtable
+            }
+
+            if ($Object -is [array]) {
                 return $Object | Sort-Object | ForEach-Object { Format-ObjectForDiff -Object $_ }
             }
-            else {
-                return $Object
-            }
+
+            return $Object
         }
     }
     process {
         try {
-            $referenceObjectTempFile = New-TemporaryFile
-            $differenceObjectTempFile = New-TemporaryFile
+            $referenceObjectTempFile = New-TemporaryFile -WhatIf:$false -Confirm:$false
+            $differenceObjectTempFile = New-TemporaryFile -WhatIf:$false -Confirm:$false
 
             if ($ReferenceObject -isnot [string]) {
                 $ReferenceObject = Format-ObjectForDiff $ReferenceObject | ConvertTo-Json -Depth 100
@@ -58,8 +70,8 @@ function Compare-ObjectGitDiff {
                 $DifferenceObject = Format-ObjectForDiff $DifferenceObject | ConvertTo-Json -Depth 100
             }
 
-            $ReferenceObject | Set-Content -Path $referenceObjectTempFile -Force
-            $DifferenceObject | Set-Content -Path $differenceObjectTempFile -Force
+            $ReferenceObject | Set-Content -Path $referenceObjectTempFile -Force -WhatIf:$false -Confirm:$false
+            $DifferenceObject | Set-Content -Path $differenceObjectTempFile -Force -WhatIf:$false -Confirm:$false
 
             $output = git diff --color --minimal --no-index $referenceObjectTempFile $differenceObjectTempFile | Out-String
             # Replace top 4 lines showing file paths and other metadata
@@ -72,8 +84,8 @@ function Compare-ObjectGitDiff {
             $output
         }
         finally {
-            Remove-Item -Path $referenceObjectTempFile -Force
-            Remove-Item -Path $differenceObjectTempFile -Force
+            Remove-Item -Path $referenceObjectTempFile -Force -WhatIf:$false -Confirm:$false
+            Remove-Item -Path $differenceObjectTempFile -Force -WhatIf:$false -Confirm:$false
         }
     }
 }
